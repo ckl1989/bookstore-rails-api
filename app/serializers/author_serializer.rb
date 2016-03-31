bc35262a9d2a0db33d623537d00b0b66954932f6
Chris Lam <christopher.kong.lam@gmail.com>
2016-03-30 18:37:09 -0700

    First commit with added rails models and serializsers

---
+++ app/serializers/author_serializer.rb
@@ -0,0 +1,7 @@
+
+class AuthorSerializer < ActiveModel::Serializer
+  attributes :id, :name, :discount
+  has_many :books
+  has_many :published
+end
+
---
+++ app/serializers/book_serializer.rb
@@ -0,0 +1,8 @@
+
+class BookSerializer < ActiveModel::Serializer
+    attributes :id, :title, :price
+    belongs_to :author
+    belongs_to :publisher
+
+end
+
